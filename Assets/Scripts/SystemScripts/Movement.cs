2562a365358db467e02687be2c9cd0d7bca403b5
ArmandGemeux <armandgemeux@hotmail.fr>
2021-05-25 09:43:19 +0200

    Push Anti-Andréa bis : Protection contre déplacement buggé

--- Assets/Scripts/SystemScripts/Movement.cs
+++ Assets/Scripts/SystemScripts/Movement.cs
@@ -5,7 +5,6 @@ using UnityEngine;
 public class Movement : MonoBehaviour
 {
     private Vector3 mousePosition;
-    private float moveSpeed = 2;
 
     private bool isInsideMapLimits = true;
     private bool isInAnObstacle = false;
@@ -65,7 +64,7 @@ public class Movement : MonoBehaviour
 
             mousePosition = Input.mousePosition;
             mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
-            transform.position = Vector2.Lerp(transform.position, mousePosition, moveSpeed);
+            transform.position = Vector2.Lerp(transform.position, mousePosition, 0.15f);
 
             foreach (FideleManager fmir in myFM.unitsInRange)
             {
